939ebbde47e4fc80cc787b0b8f14f019aa754192
Xopsa <56687497+Xopsa@users.noreply.github.com>
2019-12-10 00:29:04 +0100

    keine ahnung

--- Snips_schlagwahl.py
+++ Snips_schlagwahl.py
@@ -19,16 +19,17 @@ def schlagAuswahl(hermes, intent_message):
     schlag = intentMessage.slots.schlag.first().value
     result_sentence = "Die Auswahl ist: {}".format(str(schlag)
     #print("Schlag ausm GitHub ausgeführt: {}!!!".format(str(schlag)))
-    current_session_id = intent_message.session_id
-    hermes.publish_end_session(current_session_id, result_sentence)
+    current_session = intent_message.session_id
+    hermes.publish_end_session(current_session, result_sentence)
+
 
 def farbAuswahl(hermes, intent_message):
     intentname = intent_message.intent.intent_name
     farbe = intentMessage.slots.farbe.first().value
     result_sentence = "Die Auswahl ist: {}".format(str(farbe))
     #print("Farbe ausm GitHub ausgeführt: {}!!!".format(str(farbe)))
-    current_session_id = intent_message.session_id
-    hermes.publish_end_session(current_session_id, result_sentence)
+    current_session = intent_message.session_id
+    hermes.publish_end_session(current_session, result_sentence)
 
 if __name__ == "__main__":
     snips_config = toml.load('/etc/snips.toml')
